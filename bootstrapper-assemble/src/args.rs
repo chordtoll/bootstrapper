a7f791728d1e99a149e04c13d3eaac0c3f2ade4b
chordtoll <git@chordtoll.com>
2024-03-17 17:02:58 -0700

    refactor: break out build driver, add --no-checksum flag

--- bootstrapper-assemble/src/args.rs
+++ bootstrapper-assemble/src/args.rs
@@ -1,21 +1,20 @@
 use clap::Parser;
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    /// Rebuild images for which we already have a container
     #[arg(short, long)]
     pub rebuild: bool,
 
-    /// Compare before and after build steps
     #[arg(short, long)]
     pub diff: bool,
 
-    /// Push built images to the registry
     #[arg(short, long)]
     pub push: bool,
 
+    #[arg(short, long)]
+    pub no_checksum: bool,
+
     #[arg(last = true)]
-    pub target: String,
+    pub target: Option<String>,
 }
